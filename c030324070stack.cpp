f926710cc6972ecd4c30a2167982f5a6e05defc2
Zaidan221 <zaidansaputra216@gmail.com>
2025-06-03 09:09:32 +0800

    commit ulang struktur dasar stack dan deklarasi fungsi

--- c030324070stack.cpp
+++ c030324070stack.cpp
@@ -1,98 +1,19 @@
+#include <iostream>
+using namespace std;
+#include <string>
 
-#include<iostream>
-#include<memory>
+typedef string ItemType;  
+#define MAXSTACK 5
 
-// Init bagian data
-using itemType = int;
-const size_t MAXSTACK = 10;
+typedef struct {
+    ItemType Item[MAXSTACK];
+    int Count;
+} Stack;
 
-struct dataStack
-{
-  itemType item[MAXSTACK];
-  int count;
-};
-// Menggunakan unique ptr ke dataStack
-using myPtr = std::unique_ptr<dataStack>;
-
-// Init fungsi dari stack
-namespace stack
-{
-  void init(myPtr &S);
-  bool isEmpty(const myPtr &S);
-  bool isFull(const myPtr &S);
-  void pop(myPtr &S);
-  void push(myPtr &S, itemType x);
-  void display(myPtr &S);
-};
-
-
-int main()
-{
-  // Init myPtr
-  myPtr ptrStack = std::make_unique<dataStack>();
-  
-  // Program stack
-  stack::init(ptrStack);
-  stack::push(ptrStack, 5);
-  stack::push(ptrStack, 7);
-  stack::push(ptrStack, 10);
-  stack::push(ptrStack, 35);
-  stack::pop(ptrStack);
-  stack::display(ptrStack);
-  
-  return 0;
-}
-
-/* Logika fungsi untuk stack */
-inline void stack::init(myPtr &S) {S -> count = 0;}
-
-inline bool stack::isEmpty(const myPtr &S) {return S -> count == 0;}
-
-inline bool stack::isFull(const myPtr &S) {return S -> count == MAXSTACK;}
-
-
-void stack::push(myPtr &S, itemType x)
-{
-  /* 
-  Fungsi untuk push data ke stack. Dimana ada safety untuk cek apakah stacknya full atau tidak.
-  Logika yang dipakai yaitu dengan mengguankan count sebagai head dari stack.
-     */
-
-  if(stack::isFull(S)){
-    std::cout << "Stack penuh, data tidak bisa dimasukkan!" << std::endl;
-    return;
-  }
-
-  S -> item[S -> count] = x;
-  ++(S -> count);
-  std::cout << "Push data " << x << " berhasil!" << std::endl;
-}
-
-
-void stack::pop(myPtr &S)
-{
-  /*
-  Fungsi untuk pop data dari stack. Mempunyai safety untuk cek apakah stacknya kosong atau tidak.
-  Logika yang dipakai tidak secara langsung menghapus data dari head sebelumnya.
-  Akan tetapi mengurangi count ke stack sebelumnya.
-  */
-
-  if (stack::isEmpty(S)){
-    std::cout << "Stack masih kosong!" << std::endl;
-    return;
-  }
-
-  --(S -> count);
-  itemType dataPopped = S -> item[S -> count];
-  std::cout << "Pop data " << dataPopped << " berhasil!" << std::endl;
-}
-
-
-void stack::display(myPtr &S)
-{
-  // Menampilkan semua isi dari item di stack
-  std::cout << "Menampilkan Data:\n";
-  for(int i = (S -> count) - 1; i >= 0; --i){
-    std::cout << S -> item[i] << std::endl;
-  }
-}
+// Deklarasi fungsi
+void InitializeStack(Stack *S);
+int Empty(Stack *S);
+int Full(Stack *S);
+void Push(ItemType x, Stack *S);
+void Pop(Stack *S, ItemType *x);
+void Display(Stack *S);
