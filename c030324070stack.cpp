f0f8f34cdcfe94008f95e8aba8bc272e2fcc252e
Zaidan221 <zaidansaputra216@gmail.com>
2025-06-03 12:32:44 +0800

    tambah library dan tipe data

--- c030324070stack.cpp
+++ c030324070stack.cpp
@@ -2,18 +2,8 @@
 using namespace std;
 #include <string>
 
+// Tipe data item pada stack
 typedef string ItemType;  
+
+// Ukuran maksimum stack
 #define MAXSTACK 5
-
-typedef struct {
-    ItemType Item[MAXSTACK];
-    int Count;
-} Stack;
-
-// Deklarasi fungsi
-void InitializeStack(Stack *S);
-int Empty(Stack *S);
-int Full(Stack *S);
-void Push(ItemType x, Stack *S);
-void Pop(Stack *S, ItemType *x);
-void Display(Stack *S);
